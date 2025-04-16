11863c024abc89d7057a171fecea492ee024eb67
yh97yhyh <yh97yhyh@naver.com>
2025-04-16 17:34:26 +0900

    feat: Add Inventory

---
+++ StardewMaker/Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class InventorySlot
+{
+    public ItemData itemData;
+    public int quantity;
+
+    public bool IsEmpty()
+    {
+        return itemData == null;
+    }
+
+    public void Clear()
+    {
+        itemData = null;
+        quantity = 0;
+    }
+}
