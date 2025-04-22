35c7e95c64963c9db66cfd3d1bf4b414298b0d20
yh97yhyh <yh97yhyh@naver.com>
2025-04-22 15:39:32 +0900

    fix: Update transparent detection

--- StardewMaker/Assets/Scripts/Util/TransparentDetection.cs
+++ StardewMaker/Assets/Scripts/Util/TransparentDetection.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TransparentDetection : MonoBehaviour
 {
@@ -8,17 +9,26 @@ public class TransparentDetection : MonoBehaviour
     [SerializeField] private float fadeTime = 0.4f;
 
     private SpriteRenderer sr;
+    private Tilemap tm;
 
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
+        tm = GetComponent<Tilemap>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>())
         {
-            StartCoroutine(FadeRoutine(sr, fadeTime, sr.color.a, transparencyAmount));
+            if (sr)
+            {
+                StartCoroutine(FadeRoutine(sr, fadeTime, sr.color.a, transparencyAmount));
+            }
+            else if (tm)
+            {
+                StartCoroutine(FadeRoutine(tm, fadeTime, tm.color.a, transparencyAmount));
+            }
         }
     }
 
@@ -26,8 +36,16 @@ public class TransparentDetection : MonoBehaviour
     {
         if (collision.gameObject.GetComponent<PlayerController>())
         {
-            StartCoroutine(FadeRoutine(sr, fadeTime, sr.color.a, 1f));
+            if (sr)
+            {
+                StartCoroutine(FadeRoutine(sr, fadeTime, sr.color.a, 1f));
+            }
+            else if (tm)
+            {
+                StartCoroutine(FadeRoutine(tm, fadeTime, tm.color.a, 1f));
+            }
         }
+
     }
 
     private IEnumerator FadeRoutine(SpriteRenderer sr, float fadeTime, float startValue, float targetTransparency)
@@ -38,6 +56,18 @@ public class TransparentDetection : MonoBehaviour
             elapsedTime += Time.deltaTime;
             float newAlpha = Mathf.Lerp(startValue, targetTransparency, elapsedTime / fadeTime);
             sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, newAlpha);
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeRoutine(Tilemap tm, float fadeTime, float startValue, float targetTransparency)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startValue, targetTransparency, elapsedTime / fadeTime);
+            tm.color = new Color(tm.color.r, tm.color.g, tm.color.b, newAlpha);
             yield return null;
         }
     }
